6ab5ff6812d986dd71f828afc9a0a8694cd8965a
Administrator <83489482@qq.com>
2021-10-29 00:15:32 +0800

    我提交的内容

--- src/main/java/cn/bdqn/StuSysJavaApplication.java
+++ src/main/java/cn/bdqn/StuSysJavaApplication.java
@@ -7,7 +7,7 @@ import org.springframework.context.ConfigurableApplicationContext;
 
 @SpringBootApplication
 public class StuSysJavaApplication {
-
+//这是我的注释
     public static void main(String[] args) {
         SpringApplication.run(StuSysJavaApplication.class, args);
     }
