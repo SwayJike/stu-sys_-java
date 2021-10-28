297962fedb00eb53f7134d7452398042411efc65
SwayJike <848988457@qq.com>
2021-10-29 00:36:40 +0800

    three commit

--- src/main/java/cn/bdqn/StuSysJavaApplication.java
+++ src/main/java/cn/bdqn/StuSysJavaApplication.java
@@ -7,8 +7,6 @@ import org.springframework.context.ConfigurableApplicationContext;
 
 @SpringBootApplication
 public class StuSysJavaApplication {
-    //这个注释别动
-    //这是JIKE的注释
     public static void main(String[] args) {
         SpringApplication.run(StuSysJavaApplication.class, args);
     }
